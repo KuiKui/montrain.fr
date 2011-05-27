10b8e61217816e4bf994280baa4171c63ae59dd7
KuiKui <denis.roussel@gmail.com>
2011-05-27 12:46:16 +0200

    adding a new task completely removing the cache except the .gitignore file

---
+++ lib/task/kCacheClearTask.class.php
@@ -0,0 +1,43 @@
+<?php
+
+class kCacheClearTask extends sfBaseTask
+{
+  /**
+   * @return void
+   */
+  protected function configure()
+  {
+    $this->namespace = 'k';
+    $this->name = 'clear-cache';
+    $this->aliases = array('kcc');
+    $this->briefDescription = 'Complete removal of cache (.gitignore file is not erased)';
+  }
+
+  /**
+   * @param array $arguments
+   * @param array $options
+   * @return int
+   */
+  protected function execute($arguments = array(), $options = array())
+  {
+    if (!sfConfig::get('sf_cache_dir') || !is_dir(sfConfig::get('sf_cache_dir')))
+    {
+      throw new sfException(sprintf('Cache directory "%s" does not exist.', sfConfig::get('sf_cache_dir')));
+    }
+    
+    try
+    {
+      $this->getFilesystem()->remove(sfFinder::type('any')
+        ->discard('.sf')
+        ->discard('.gitignore')
+        ->in(sfConfig::get('sf_cache_dir'))
+      );
+    }
+    catch(Exception $e)
+    {
+      return 1;
+    }
+
+    return 0;
+  }
+}
